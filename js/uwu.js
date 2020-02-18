d1940448eaac4b8183d70f5cb1e665e0566d0857
Aaron Jumarang <kapj.jumarang@gmail.com>
2020-02-17 22:16:19 -0500

    added table

--- js/uwu.js
+++ js/uwu.js
@@ -16,9 +16,11 @@ function convertToCSV(objArray) {
     return str;
 }
 
+var headerCount = 1;
 function exportCSVFile(headers, items, fileTitle) {
-    if (headers) {
+    if (headers && headerCount==1) {
         items.unshift(headers);
+        headerCount++;
     }
 
     // Convert Object to JSON
@@ -134,7 +136,6 @@ var fileTitle = 'Scout-entry'; // or 'my-unique-title'
 // 		"crossedinitiation": "Yes"
 // };
 
-
 function add(lmao) {
 	// // adder = document.getElementById("myInput").value;
 	// // alert(adder);
@@ -175,8 +176,64 @@ function add(lmao) {
 	    groundPCP: String(item.groundPCP),
 	    loadingPCP: String(item.loadingPCP)
     });	
+    displayTable();
 }
 
 function download() {
 	exportCSVFile(headers, itemsFormatted, fileTitle); // call the exportCSVFile() function to process the JSON and trigge
+}
+
+function getData() {
+    var hehe = convertToCSV(JSON.stringify(itemsFormatted));
+    var rawData = new Blob([hehe], { type: 'text/csv;charset=utf-8;' });
+    return rawData;
+}
+
+function reset() {
+    itemsFormatted = [];
+    headerCount = 1;
+    entries = 0;
+    document.getElementById("entry").innerHTML = '';
+    document.getElementById("showData").innerHTML = '';
+}
+
+function displayTable(){
+    var col = [];
+        for (var i = 0; i < itemsFormatted.length; i++) {
+            for (var key in itemsFormatted[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < 30; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+            console.log(i);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < itemsFormatted.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = itemsFormatted[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
 }
